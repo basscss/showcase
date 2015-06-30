c2275e68d95b696960c0aa118c21299127ba7f9b
jxnblk <jacksonblack@gmail.com>
2015-06-30 10:12:10 -0400

    React build

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+
+var StaticSiteGeneratorPlugin = require('static-site-generator-webpack-plugin')
+var data = require('./data')
+
+module.exports = {
+  entry: './entry.js',
+
+  output: {
+    filename: 'bundle.js',
+    path: __dirname,
+    libraryTarget: 'umd'
+  },
+
+  module: {
+    loaders: [
+      { test: /(\.js$|\.jsx$)/, loader: 'babel-loader' },
+      { test: /\.json/, loader: 'json-loader' },
+      { test: /\.css$/, loader: 'css-loader!cssnext-loader' },
+      { test: /\.png$/, loader: 'file-loader?name=/images/[hash].[ext]' }
+    ]
+  },
+
+  cssnext: {
+    compress: true
+  },
+
+  plugins: [
+    new StaticSiteGeneratorPlugin('bundle.js', ['/'], data)
+  ]
+}
+
